d514d6fd8b78e1a687b0e9fb7ba82b072d4a64f4
Agnieszka <145773068+aerien33@users.noreply.github.com.>
2024-01-18 16:30:08 +0100

    POST ingredient to database

--- server.js
+++ server.js
@@ -7,91 +7,99 @@ var db = mongoose.connect('mongodb://localhost/meal-planner-test');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var ingredients = [
-    {
-        "_id": "38439438934",
-        "title": "marchew",
-        "quantity": 90,
-        "unit": "g"
-    },
-    {
-        "_id": "37378474343",
-        "title": "pietruszka (korzeń)",
-        "quantity": 80,
-        "unit": "g"
-    }
-];
+var Ingredient = require('./model/ingredient');
+
+//var ingredients = [
+//    {
+//        "_id": "38439438934",
+//        "title": "marchew",
+//        "quantity": 90,
+//        "unit": "g"
+//    },
+//    {
+//        "_id": "37378474343",
+//        "title": "pietruszka (korzeń)",
+//        "quantity": 80,
+//        "unit": "g"
+//    }
+//];
 
 
-app.get('/ingredients', function(request, response) {
-    response.status(200).send(ingredients);
-});
+//app.get('/ingredients', function(request, response) {
+//    response.status(200).send(ingredients);
+//});
 
 
-app.post('/ingredients', function(request, response) {
-    var ingredient = request.body;
-    
-    if (!ingredient || ingredient.title === "") {
-        response.status(500).send({error: "Provide a title for the ingredient"});
-    } else {
-        ingredients.push(ingredient);
-        response.status(200).send(ingredients);
-    }
-});
-
-
-app.put('/ingredients/:_id', function(request, response) {
-    
-    var newTitle = request.body.title;
-    
-    if (!newTitle || newTitle === "") {
-        response.status(500).send({error:"Provide a title for the ingredient"});
-    } else {
-        var objectFound = false;
-        for (var x = 0; x < ingredients.length; x++) {
-            var ing = ingredients[x];
-            
-            if (ing._id === request.params._id) {
-                ingredients[x].title = newTitle;
-                objectFound = true;
-                break;
-            }
+app.post('/ingredients', async (request, response) => {
+    try {
+        const ingredient = await new Ingredient();
+        ingredient.title = request.body.title;
+        ingredient.quantity = request.body.quantity;
+        
+        if(!request.unit) {
+            ingredient.unit = request.body.unit;
         }
         
-        if (!objectFound) {
-            response.status(500).send({error:"Ingredient id was not found"});
-        } else {
-            response.send(ingredients);
-        }
+        ingredient.save();
+        response.status(200).send(ingredient);
+    } catch {
+        response.status(500).send({error:"Could not save ingredient"});
     }
 });
 
 
-app.delete('/ingredients/:_id', function(request, response) {
-           
-    var objectFound = false;
-    for (var x = 0; x < ingredients.length; x++) {
-        var ing = ingredients[x];
+//app.put('/ingredients/:_id', function(request, response) {
+//    
+//    var newTitle = request.body.title;
+//    
+//    if (!newTitle || newTitle === "") {
+//        response.status(500).send({error:"Provide a title for the ingredient"});
+//    } else {
+//        var objectFound = false;
+//        for (var x = 0; x < ingredients.length; x++) {
+//            var ing = ingredients[x];
+//            
+//            if (ing._id === request.params._id) {
+//                ingredients[x].title = newTitle;
+//                objectFound = true;
+//                break;
+//            }
+//        }
+//        
+//        if (!objectFound) {
+//            response.status(500).send({error:"Ingredient id was not found"});
+//        } else {
+//            response.send(ingredients);
+//        }
+//    }
+//});
 
-        if (ing._id === request.params._id) {
-            var index = ingredients.indexOf(ing);
-            if (index > -1) {
-                ingredients.splice(index, 1);
-            } else {
-                console.log("Could not remove the ingredient");
-            }
-            objectFound = true;
-            break;
-        }
-    }
-        
-    if (!objectFound) {
-        response.status(500).send({error:"Ingredient id not found"})
-    } else {
-        response.send(ingredients);
-    }
-    
-});
+
+//app.delete('/ingredients/:_id', function(request, response) {
+//           
+//    var objectFound = false;
+//    for (var x = 0; x < ingredients.length; x++) {
+//        var ing = ingredients[x];
+//
+//        if (ing._id === request.params._id) {
+//            var index = ingredients.indexOf(ing);
+//            if (index > -1) {
+//                ingredients.splice(index, 1);
+//            } else {
+//                console.log("Could not remove the ingredient");
+//            }
+//            objectFound = true;
+//            break;
+//        }
+//    }
+//        
+//    if (!objectFound) {
+//        response.status(500).send({error:"Ingredient id not found"})
+//    } else {
+//        response.send(ingredients);
+//    }
+//    
+//});
 
 
 app.listen(3004, function() {
